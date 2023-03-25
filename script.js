37fcac13ed51b1f59118a3110706c6470c187f1a
BigBobandy <jdykes207@gmail.com>
2023-03-25 15:35:06 -0400

    added function for getting user input
    I changed the convertCurrency function to getInput. This functions gets input from the user and checks if it's valid. Also added documentation explaining different parts of the function.

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const amountInput = document.getElementById("amount");
-const baseCurrency = document.getElementById("base-currency").value;
-const targetCurrency = document.getElementById("target-currency").value;
+const baseCurrencySelect = document.getElementById("base-currency");
+const targetCurrencySelect = document.getElementById("target-currency");
 const convertBtn = document.getElementById("convert-btn");
 const resultElement = document.getElementById("result");
 
-convertBtn.addEventListener("click", () => {
-  convertCurrency();
-});
+convertBtn.addEventListener("click", getInput);
 
-function convertCurrency() {
+function getInput() {
+  //Getting the value of the input field
   const amountInputValue = amountInput.value.trim();
 
+  //Getting the selected base and target currencies
+  const baseCurrency = baseCurrencySelect.value;
+  const targetCurrency = targetCurrencySelect.value;
+
+  //Checking if the selected currencies are the same. Returns early if so
+  if (baseCurrency === targetCurrency) {
+    console.log("Base currency and target currency cannot be the same");
+    return;
+  }
+
+  //Input validation to ensure that the input field isn't blank or not a number
   if (amountInputValue === "" || isNaN(amountInputValue)) {
     console.log("Invalid input value");
   } else {
+    //Taking the input value, parsing it as a number and assigning it to the amount variable
     const amount = parseFloat(amountInputValue);
     console.log("Amount:", amount);
+    console.log("Base currency:", baseCurrency);
+    console.log("Target currency:", targetCurrency);
   }
 }
 
